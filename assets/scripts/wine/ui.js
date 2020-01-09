d9e9914e0f2600972b9213ed13271eee5593c045
yingsongsana <yingsongsana@gmail.com>
2020-01-09 14:57:44 -0500

    Added styling for wine cards & media queries

--- assets/scripts/wine/ui.js
+++ assets/scripts/wine/ui.js
@@ -17,7 +17,9 @@ const failureMessage = function (newText) {
 const displayWine = function (responseData) {
   $('.wine-cellar').text('')
   const wineText = (`
-    <div class="containerSingle">
+    <div class="container">
+    <div class="card">
+    <div class="card-body">
     <h3>${responseData.wine.name}</h3>
     <ul>ID: ${responseData.wine.id}</ul>
     <ul>Producer: ${responseData.wine.producer}</ul>
@@ -29,6 +31,8 @@ const displayWine = function (responseData) {
     <ul>Aroma: ${responseData.wine.aroma}</ul>
     <ul>Tasting Notes: ${responseData.wine.tasting_notes}</ul>
     <ul>Memories: ${responseData.wine.memories}</ul>
+    </div>
+    </div>
     </div>
   `)
   $('.wine-cellar').append(wineText)
